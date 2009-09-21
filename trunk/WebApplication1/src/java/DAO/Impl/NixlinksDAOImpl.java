1d0c3136a7dd5a20b276a41dc7d161e4a23690ac
andrey1602@mail.ru <andrey1602@mail.ru@0e7f9a46-a683-11de-b1fe-bba1d6bec496>
2009-09-21 13:12:21 +0000

    Rev1

--- trunk/WebApplication1/src/java/DAO/Impl/NixlinksDAOImpl.java
+++ trunk/WebApplication1/src/java/DAO/Impl/NixlinksDAOImpl.java
@@ -2,13 +2,36 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package DAO.Impl;
+
+import DAO.NixlinksDAO;
+import Pojo.Nixlinks;
+import hUtil.HibernateUtil;
+import java.sql.SQLException;
+import org.hibernate.Session;
 
 /**
  *
  * @author Admin4DB2
  */
-public class NixlinksDAOImpl {
+public class NixlinksDAOImpl implements NixlinksDAO {
 
+    public void addNixlink(Nixlinks nixlinks) throws SQLException {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(nixlinks);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+    }
+
+    public void addCurrency(Nixlinks nixlinks) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
