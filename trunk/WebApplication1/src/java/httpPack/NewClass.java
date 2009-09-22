51cfe4046066881385ad4628c1a1a66d74202b5b
andrey1602@mail.ru <andrey1602@mail.ru@0e7f9a46-a683-11de-b1fe-bba1d6bec496>
2009-09-22 08:22:38 +0000

    Rev1

--- trunk/WebApplication1/src/java/httpPack/NewClass.java
+++ trunk/WebApplication1/src/java/httpPack/NewClass.java
@@ -22,7 +22,7 @@ public class NewClass {
 //        out.close();
 //        File outFile1 = new File("c://out1.xml");
 //        FileWriter out1 = new FileWriter(outFile1);
-      //        hdao.DownloadContentPT("http://www.nix.ru/price/price.html");
+  //           hdao.DownloadContentPT("http://www.nix.ru/price/price.html");
 //        out1.close();
         hdao.DownloadContentPTURL(hdao.DownloadContentPT("http://www.nix.ru/price/price.html"));
         //hdao.DownloadContentCard();
