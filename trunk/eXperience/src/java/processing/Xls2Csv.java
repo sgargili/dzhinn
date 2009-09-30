b2192b280c822cf0c4ab60a2db3dbea61edb0f95
andrey1602@mail.ru <andrey1602@mail.ru@0e7f9a46-a683-11de-b1fe-bba1d6bec496>
2009-09-30 14:26:15 +0000

    Rev1

---
+++ trunk/eXperience/src/java/processing/Xls2Csv.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package processing;
+
+/**
+ *
+ * @author APopov
+ */
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.commons.io.*;
+
+public class Xls2Csv {
+
+    public File convert(InputStream uploadFile, String fileName, String encoding) throws IOException {
+        File file = new File("temp.csv");
+        IOUtils.copyLarge(uploadFile, FileUtils.openOutputStream(file));
+        File file2 = new File(fileName);
+        List lines = FileUtils.readLines(file, encoding);
+        String re;
+        Pattern p;
+        Matcher m;
+        int i = 0;
+        for (Iterator it = lines.iterator(); it.hasNext();) {
+            String str = (String) it.next();
+            str = str.replaceAll("\"", "\"\"");
+            str = str.replaceAll(";", "\",\"");
+            re = "(^.)";
+            p = Pattern.compile(re);
+            m = p.matcher(str);
+            if (m.find()) {
+                str = m.replaceAll("\"$1");
+            }
+            re = "(.$)";
+            p = Pattern.compile(re);
+            m = p.matcher(str);
+            if (m.find()) {
+                str = m.replaceAll("$1\"");
+            }
+            str = str.trim();
+            lines.set(i, str);
+            i++;
+        }
+        FileUtils.writeLines(file2, encoding, lines);
+        return file2;
+    }
+}
+
