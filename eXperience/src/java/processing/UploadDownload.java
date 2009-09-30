1b5bf8436326a7a063a89f689aff5e708c235cb1
andrey1602@mail.ru <andrey1602@mail.ru@0e7f9a46-a683-11de-b1fe-bba1d6bec496>
2009-09-30 14:26:15 +0000

    Rev1

--- eXperience/src/java/processing/UploadDownload.java
+++ eXperience/src/java/processing/UploadDownload.java
@@ -3,10 +3,14 @@ package processing;
 import Beans.ManPTCreator;
 import Beans.fCreator;
 import httpClientPack.HttpClientManPT;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.zip.ZipException;
+import org.apache.commons.io.FileUtils;
+import org.directwebremoting.io.FileTransfer;
 import org.xmlpull.v1.XmlPullParserException;
 
 public class UploadDownload {
@@ -33,6 +37,14 @@ public class UploadDownload {
         String temp = updManPt.getManPt();
         Long time2 = System.currentTimeMillis();
         return temp;
-       // return "Обновлено за: " + (time2 - time1) / 1000 + " сек.";
+        // return "Обновлено за: " + (time2 - time1) / 1000 + " сек.";
+    }
+
+    public FileTransfer convertXLSCSV(InputStream uploadFile, String fileName, String encoding) throws Exception {
+        Xls2Csv x2c = new Xls2Csv();
+        File fl = x2c.convert(uploadFile, fileName, encoding);
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        buffer.write(FileUtils.readFileToByteArray(fl));
+        return new FileTransfer(fileName, "", buffer.toByteArray());
     }
 }
