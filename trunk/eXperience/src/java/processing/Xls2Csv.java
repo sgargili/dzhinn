afa1fa51663bdb23548113d60a26a10a4a4703ac
andrey1602@mail.ru <andrey1602@mail.ru@0e7f9a46-a683-11de-b1fe-bba1d6bec496>
2009-10-01 12:43:33 +0000

    Rev1

--- trunk/eXperience/src/java/processing/Xls2Csv.java
+++ trunk/eXperience/src/java/processing/Xls2Csv.java
@@ -8,19 +8,32 @@ package processing;
  *
  * @author APopov
  */
+import csvprocessing.CsvWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
 import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import jxl.Sheet;
+import jxl.Workbook;
+import org.apache.commons.compress.archivers.ArchiveException;
+import org.apache.commons.compress.archivers.ArchiveOutputStream;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
 import org.apache.commons.io.*;
 
 public class Xls2Csv {
 
-    public File convert(InputStream uploadFile, String fileName, String encoding, String checkSeparator, String checkZip) throws IOException {
+    public File convertCsv2Csv(InputStream uploadFile, String fileName, String encoding, String checkSeparator, String checkZip) throws IOException, ArchiveException {
+        long start = System.currentTimeMillis();
         File file = new File("temp.csv");
+        File outputFile = new File(fileName + ".zip");
         boolean sepbool = false, zipbool = false;
         if (checkSeparator.equals("true")) {
             sepbool = true;
@@ -69,9 +82,65 @@ public class Xls2Csv {
         }
         FileUtils.writeLines(file2, encoding, lines);
         if (zipbool) {
-            
+            final OutputStream out = new FileOutputStream(outputFile);
+            ArchiveOutputStream os = new ArchiveStreamFactory().createArchiveOutputStream("zip", out);
+            os.putArchiveEntry(new ZipArchiveEntry(fileName + ".csv"));
+            IOUtils.copy(new FileInputStream(file2), os);
+            os.closeArchiveEntry();
+            os.close();
+            long end = System.currentTimeMillis();
+            System.out.println(end - start + " миллисекунд");
+            return outputFile;
+        } else {
+            long end = System.currentTimeMillis();
+            System.out.println(end - start + " миллисекунд");
+            return file2;
         }
-        return file2;
+    }
+
+    public File convertXls2Csv(InputStream uploadFile, String fileName, String encoding, String checkSeparator, String checkZip) throws IOException, ArchiveException {
+        long start = System.currentTimeMillis();
+        File file = new File("temp.xls");
+        File outputFile = new File(fileName + ".zip");
+        boolean zipbool = false;
+        if (checkZip.equals("true")) {
+            zipbool = true;
+        }
+        IOUtils.copyLarge(uploadFile, FileUtils.openOutputStream(file));
+        File file2 = new File(fileName);
+        CsvWriter csvw = new CsvWriter(file2.getAbsolutePath(), ',', Charset.forName(encoding));
+        try {
+            Workbook wb = Workbook.getWorkbook(file);
+            Sheet sheet = wb.getSheet(0);
+            int rows = sheet.getRows();
+            int columns = sheet.getColumns();
+            String[] temp = new String[columns];
+            for (int row = 1; row < rows; row++) {
+                for (int column = 1; column < columns; column++) {
+                    temp[column] = sheet.getCell(column, row).getContents();
+                }
+                csvw.writeRecord(temp);
+            }
+            csvw.close();
+            wb.close();
+        } catch (Exception ioe) {
+            System.out.println("Error: " + ioe);
+        }
+        if (zipbool) {
+            final OutputStream out = new FileOutputStream(outputFile);
+            ArchiveOutputStream os = new ArchiveStreamFactory().createArchiveOutputStream("zip", out);
+            os.putArchiveEntry(new ZipArchiveEntry(fileName + ".csv"));
+            IOUtils.copy(new FileInputStream(file2), os);
+            os.closeArchiveEntry();
+            os.close();
+            long end = System.currentTimeMillis();
+            System.out.println(end - start + " миллисекунд");
+            return outputFile;
+        } else {
+            long end = System.currentTimeMillis();
+            System.out.println(end - start + " миллисекунд");
+            return file2;
+        }
     }
 }
 
