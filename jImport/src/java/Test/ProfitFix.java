7c1afd415ef2d0f5bd1a1d4b7cbba3717357559a
andrey1602@mail.ru <andrey1602@mail.ru@0e7f9a46-a683-11de-b1fe-bba1d6bec496>
2009-11-22 19:37:30 +0000

    First Version

--- jImport/src/java/Test/ProfitFix.java
+++ jImport/src/java/Test/ProfitFix.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package Test;
 
 import CSV.CsvReader;
@@ -24,16 +20,19 @@ public class ProfitFix {
         String[] mass = new String[4];
         reader.readHeaders();
         int i = 1;
+//        Pattern p;
+//        Matcher m;
+        String ss1 = "", ss2 = "";
         while (reader.readRecord()) {
-            Pattern p = Pattern.compile("(.{60,100}){2}");
-            Matcher m = p.matcher(reader.get(3));
-            if (m.find() && reader.get(1).trim().equals("MICROSOFT")) {
+            //  p = Pattern.compile("(.{" + reader.get(3).trim().length() / 2 + "}+){2}");
+            //m = p.matcher(reader.get(3));
+            ss1 = reader.get(3).trim().substring(0, reader.get(3).trim().length() / 2);
+            ss2 = reader.get(3).trim().substring(reader.get(3).trim().length() / 2);
+            if (ss1.equals(ss2) && reader.get(1).trim().equals("MICROSOFT")) {
                 mass[0] = reader.get(1).trim();
                 mass[1] = reader.get(2).trim();
                 mass[2] = reader.get(3).trim();
-                mass[3] = m.group(1);
-//                System.out.println(m.group(1));
-//                System.out.println(i++ + " " + reader.get(1).trim() + " " + reader.get(2).trim() + " " + reader.get(3).trim());
+                mass[3] = ss1;
                 writer.writeRecord(mass);
                 writer.flush();
             }
