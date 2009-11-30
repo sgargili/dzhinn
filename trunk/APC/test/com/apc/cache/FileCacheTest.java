1658f214600e138243d589bf6480b423b57a14bf
andrey1602@mail.ru <andrey1602@mail.ru@0e7f9a46-a683-11de-b1fe-bba1d6bec496>
2009-11-30 23:31:20 +0000

    First Version

---
+++ trunk/APC/test/com/apc/cache/FileCacheTest.java
@@ -0,0 +1,89 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.apc.cache;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Admin4DB2
+ */
+public class FileCacheTest {
+
+    public FileCacheTest() {
+    }
+
+    /**
+     * Test of clearAllFromFile method, of class FileCache.
+     */
+    @Test
+    public void testClearAllFromFile() {
+        System.out.print("FileCache.clearAllFromFile Testing... ---> ");
+        FileCache instance = FileCache.getInstance();
+        instance.clearAllFromFile();
+        System.out.println("Done");
+
+    }
+
+    /**
+     * Test of containsInFile method, of class FileCache.
+     */
+    @Test
+    public void testContainsInFile() {
+        System.out.print("FileCache.containsInFile Testing... ---> ");
+        String key = "testKey";
+        FileCache instance = FileCache.getInstance();
+        boolean expResult = false;
+        boolean result = instance.containsInFile(key);
+        assertEquals(expResult, result);
+        System.out.println("Done");
+    }
+
+    /**
+     * Test of putInFile method, of class FileCache.
+     */
+    @Test
+    public void testPutInFile() {
+        System.out.print("FileCache.putInFile Testing... ---> ");
+        String key = "testKey";
+        String value = "testValue";
+        FileCache instance = FileCache.getInstance();
+        instance.putInFile(key, value);
+        boolean expResult = true;
+        boolean result = instance.containsInFile(key);
+        assertEquals(expResult, result);
+        System.out.println("Done");
+    }
+
+    /**
+     * Test of getFromFile method, of class FileCache.
+     */
+    @Test
+    public void testGetFromFile() {
+        System.out.print("FileCache.getFromFile Testing... ---> ");
+        String key = "testKey";
+        FileCache instance = FileCache.getInstance();
+        String expResult = "testValue";
+        String result = instance.getFromFile(key);
+        assertEquals(expResult, result);
+        System.out.println("Done");
+    }
+
+    /**
+     * Test of clearFromFile method, of class FileCache.
+     */
+    @Test
+    public void testClearFromFile() {
+        System.out.print("FileCache.clearFromFile Testing... ---> ");
+        String key = "testKey";
+        FileCache instance = FileCache.getInstance();
+        instance.clearFromFile(key);
+        boolean expResult = false;
+        boolean result = instance.containsInFile(key);
+        assertEquals(expResult, result);
+        System.out.println("Done");
+    }
+}
---
+++ trunk/APC/test/com/apc/processing/FileUtilsTest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.apc.processing;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Admin4DB2
+ */
+public class FileUtilsTest {
+
+    public FileUtilsTest() {
+    }
+
+    /**
+     * Test of readFileToString method, of class FileUtils.
+     */
+    @Test
+    public void testReadFileToString() {
+        System.out.print("FileUtils.readFileToString Testing... ---> ");
+        String fileName = "file4Test.data";
+        String encoding = "Cp1251";
+        int beginIndex = 6;
+        int length = 16;
+        FileUtils instance = FileUtils.getInstance();
+        String expResult = "World! New Hello";
+        String result = instance.readFileToString(fileName, encoding, beginIndex, length);
+        assertEquals(expResult, result);
+        System.out.println("Done");
+    }
+}
