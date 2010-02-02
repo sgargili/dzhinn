7ced79dec8f6873656ae6d1239eff7fc0ee4c361
andrey1602@mail.ru <andrey1602@mail.ru@0e7f9a46-a683-11de-b1fe-bba1d6bec496>
2010-02-02 09:10:19 +0000

    Rev2

--- jImport/src/java/value4it/Class4Test.java
+++ jImport/src/java/value4it/Class4Test.java
@@ -4,14 +4,8 @@
  */
 package value4it;
 
-import DAO.FactoryDAO;
-import Pojo.ValueArticle;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.sql.SQLException;
-import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.List;
-import org.xmlpull.v1.XmlPullParserException;
 
 /**
  *
@@ -19,30 +13,29 @@ import org.xmlpull.v1.XmlPullParserException;
  */
 public class Class4Test {
 
-    public static void main(String[] args) throws XmlPullParserException, UnsupportedEncodingException, IOException, InterruptedException, SQLException {
-//        long start = System.currentTimeMillis();
-//        Thread.sleep(1000);
-//        long end = System.currentTimeMillis();
-//        System.out.println(end - start);
+    public static void main(String[] args) {
 
-
-//                ValuePro vp = new ValuePro();
-//        String[] str = new String[3];
-//        str[0] = "111122233333344444445566";
-//        str[1] = "asasassssasasasasa";
-//        str[2] = "035K01222";
-//        List lst = vp.getArtclesIdByArticles(str);
-//        ValueArticle temp;
-//        for (Iterator it = lst.iterator(); it.hasNext();) {
-//            temp = (ValueArticle) it.next();
-//            System.out.println(temp.getArticle() + " -> " + temp.getArticleId()+" -> "+vp.isArticle(temp.getArticle()));
-//        }
-//        System.out.println(vp.buildResponse(lst, "Export"));
-        // vp.login();
-        //vp.setCookie();
-//        vp.setCookie();
-//         System.out.println(vp.isSessionAlive());
-//        System.out.println(vp.DownloadContentAsString("https://cf.value4it.com/cf/admin/index_research.jsp", "UTF-8"));
-//        vp.logout();
+        List lst = new ArrayList();
+        int k = 0;
+        for (int i = 1; i <= 100; i++) {
+            lst.add(i);
+        }
+        System.out.println(lst.size());
+        if (lst.size() % 10 == 0) {
+            k = lst.size() / 10;
+        } else {
+            k = lst.size() / 10 + 1;
+        }
+        System.out.println(k);
+        
+            for (int j = 0; j < k; j++) {
+                for (int l = j * 10; l < (j + 1) * 10; l++) {
+                    try{
+                    System.out.println(lst.get(l));
+                    } catch(IndexOutOfBoundsException ex){}
+                    }
+                System.out.println("Далее...");
+            }
+        
     }
 }
